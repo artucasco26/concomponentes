7c757c4508d6fe59817f57682755a67e4d495571
artucasco26 <75184331+artucasco26@users.noreply.github.com>
2021-12-20 12:31:59 -0500

    Add files via upload

---
+++ src/componentes/Dashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from './componentes/Header';
+import Iniciosesion from './componentes/Iniciosesion';
+import Footer from './componentes/Footer';
+
+const Dashboard = ()=> {
+    return (<>
+        <section className="u-clearfix u-section-1" id="sec-9a54">
+        <div className="u-clearfix u-sheet u-sheet-1" />
+        </section>
+        </>)
+}
+export default Dashboard
